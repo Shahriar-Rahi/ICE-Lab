8696db4e9e7c563d4311850afb74f5f7fd300421
Shahriar Rahi <md.alshahriarrahi@gmail.com>
2018-09-26 21:38:07 +0600

    Add files via upload

---
+++ NRZ_L.m
@@ -0,0 +1,48 @@
+clc;
+clear;
+%x = [0 1 0 0 1 0 1 0];
+bits = randi([0, 1], 1,10);
+
+T=length(bits);
+n=200;
+N=n*T;
+dt=T/N;
+t=0:dt:T;
+x=zeros(1,length(t));
+for i=0:T-1;
+    if bits(i+1)==1
+        x(i*n+1 : (i+1)*n)=1*3;
+    else
+        x(i*n+1 : (i+1)*n)=-1*3;
+    end;
+end;
+plot(t,x,'r-');
+axis([0 t(end) -5 5]);
+%grid on;
+title('NRZ-L');
+
+
+origin_bits = zeros(1, round(length(x) / n));
+
+for i = 1:length(x) / n
+    origin_bits(i) = x(i * n) / 3;
+end
+
+for i = 1:length(x) / n
+    count = 0;
+    for j = 1:n
+        if(x(((i-1) * n) + j) == 3)
+            count = count + 1;
+        end
+    end
+    if(count < n)
+        origin_bits(i) = 0;
+    else 
+        origin_bits(i) = 1;
+    end
+end
+
+disp("Randomly generated bits: ");
+disp(bits);
+disp("Bits retained from signal x: ");
+disp(origin_bits);
